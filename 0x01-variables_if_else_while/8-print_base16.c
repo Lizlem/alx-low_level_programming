d81b78ae3a2f945dde5b1445ec6f681b4070f665
Lizlem <aliziwexoki@gmail.com>
2023-03-20 17:33:53 -0700

    Alphanumeric

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints out function below
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char h;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (h = 'a' ; h <= 'f' ; h++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
