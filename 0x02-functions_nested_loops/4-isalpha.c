92c6cda0b80a2704f10a3af05e9b890bc496a4fc
Lizlem <aliziwexoki@gmail.com>
2023-03-22 12:14:31 -0700

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - prints out function as below
  *
- * 0c: paramenter to be printed
+ * @c: paramenter to be printed
  *
  * Return: 1 if true otherwise 0
  */
@@ -12,6 +12,6 @@ int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
-	else 
+	else
 		return (0);
 }
