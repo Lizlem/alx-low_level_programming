1158b167a2bc3f2f333c54c9e1c4a63c04a91b5a
Lizlem <aliziwexoki@gmail.com>
2023-03-23 04:52:57 -0700

    Positve

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - returns output as per below
+ *
+ * @i: parameter
+ *
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive", i);
+	else
+		printf("%d is negative", i);
+}
