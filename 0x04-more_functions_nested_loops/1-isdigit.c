108ad52fad13972bb653c3f92f7fbd404a508df5
Lizlem <aliziwexoki@gmail.com>
2023-03-24 15:30:51 -0700

    Digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - prints upper case letters
+ * _isdigit - prints if numbers are 0 - 9
  * @c: char to check
  *
  * Return: 0
@@ -9,7 +9,7 @@
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
