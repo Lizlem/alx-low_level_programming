4cd67c697117e95d3618954ea229e66349db38c2
Lizlem <aliziwexoki@gmail.com>
2023-03-28 17:57:41 -0700

    elements of an array of integers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @c: char to check
+ *
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array of integers
+ * @a: array name
+ * @n: number of elements in array
+ *
+ * Return: a and n
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
