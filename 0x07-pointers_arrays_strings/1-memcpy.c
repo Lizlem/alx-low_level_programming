5672adaebb89dc9ea7a902cfe28fd18789a3eaa8
Lizlem <aliziwexoki@gmail.com>
2023-04-04 12:24:41 -0700

    me mcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -16,6 +16,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	char *dest_ptr = memcpy(dest, src, sizeof(char) * n);
 
 	printf("dest_ptr: %p\n", dest_ptr);
-	printf("dest: %p\n", dest);
+	printf("    dest: %p\n", dest);
 	return (0);
 }
