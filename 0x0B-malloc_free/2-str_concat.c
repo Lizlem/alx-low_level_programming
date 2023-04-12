b1ccdf884b17f87582043139acaab77571fc276a
Lizlem <aliziwexoki@gmail.com>
2023-04-12 16:12:07 -0700

    concatenates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,17 +14,18 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int s1len = 0;
-	int s2len = 0;
+	int s1len = strlen(s1);
+	int s2len = strlen(s2);
 	int i = 0;
+	char *s = malloc(sizeof(char) * i);
+	int size = s1len + s2len +1;
 
-	s1 = malloc(sizeof(char) * i);
+	for (i = 0 ; i < s1len ; i++)
+		s[i] = s1[i];
+	for (i = 0 ; i < s2len ; i++)
+		s[s1len + i] = s2[i];
 
-	for (i = 0 ; s1[i] != '\0' ; i++)
-		s1len++;
-	for (i = 0 ; s2[i] != '\0' ; i++)
-		s2len++;
-	for (i = 0 ; i <= s2len ; i++)
-		s1[s1len + i] = s2[i];
+	s[size - 1] = '\0';
+
 	return (NULL);
 }
