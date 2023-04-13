9b12fae573500f98389f43bfa33b3b2daf497e79
Lizlem <aliziwexoki@gmail.com>
2023-04-12 17:17:20 -0700

    concatenates arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * argstostr - concatenates arguments
+ * @ac: integer
+ * @av: string
+ *
+ * Return: 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	return (0);
+}
